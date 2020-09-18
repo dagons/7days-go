0251f50f2d74042328f8861e6b9b360a29456b79
dagons <547990950@qq.com>
2020-09-18 18:08:51 +0800

    了解context包原理

--- test/main.go
+++ test/main.go
@@ -1,41 +1,8 @@
 package main
 
-import (
-	"reflect"
-)
-
-type aaa struct {
-	A int
-}
-
 func main() {
-	a := aaa{A: 1}
-	setFields(&a, 2, "A")
-	println(a.A)
-}
-
-func setFields(dst, src interface{}, name string) {
-	d := reflect.ValueOf(dst).Elem()
-	s := reflect.ValueOf(src)
-	df := d.FieldByName(name)
-	switch df.Kind() {
-	case reflect.String:
-		if v := s.String(); v != "" {
-			df.SetString(v)
-		}
-	case reflect.Int:
-		if v := s.Int(); v != 0 {
-			df.SetInt(v)
-		}
-	case reflect.Float32:
-	case reflect.Float64:
-		if v := s.Float(); v != 0 {
-			df.SetFloat(v)
-		}
-	case reflect.Uint:
-		if v := s.Uint(); v != 0 {
-			df.SetUint(v)
-		}
-		// handle other kinds
-	}
+	a := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	x := a[0:10]
+	println(len(x))
+	println(cap(x))
 }
